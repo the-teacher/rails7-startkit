1f059720108d379359c6ca5f7871ad5d975ba256
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-01-08 20:55:59 +0300

    gem config. Settings are reworked

---
+++ config/_CONFIG.rb
@@ -0,0 +1,7 @@
+app = Rails.application
+
+settings_file = "#{ app.config.root }/config/_CONFIG.yml"
+Settings.add_source!(settings_file)
+puts "Settings: #{settings_file}"
+
+Settings.reload!
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
+    require_relative '_CONFIG'
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
