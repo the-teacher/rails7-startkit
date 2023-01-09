b1b9987479e2061ad6fb91367e30bcd3be78e12c
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-01-09 09:31:44 +0300

    View improvements

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,12 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.first(20)
+    @articles = Article.first(5)
+
+    # https://freelancing-gods.com/thinking-sphinx/v5/searching.html
+    query = ThinkingSphinx::Query.escape('Article with an Index: 1')
+    @sphinx_articles = Article.search(
+      query,
+      order: 'title ASC'
+    )
   end
 end
