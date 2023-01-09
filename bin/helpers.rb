237f02bba0342fc4dd480a2869682181301b6a64
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-01-09 13:44:06 +0300

    Multiple improvements

--- bin/helpers.rb
+++ bin/helpers.rb
@@ -75,8 +75,12 @@ end
 
 def chewy_index
   puts "Indexing with CHEWY"
-  container_exec('rails', 'rake chewy:reset')
   container_exec('rails', 'rake chewy:update')
+end
+
+def chewy_reset
+  puts "Dropping CHEWY indicies"
+  container_exec('rails', 'rake chewy:reset')
 end
 
 def puma_start
