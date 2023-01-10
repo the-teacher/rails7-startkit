0f4b0381f5e679c1c53e11eca920929ff9bc540c
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-01-10 23:32:57 +0300

    Improve articles#index vuiew

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.first(5)
+    @articles = Article.first(3)
 
     # https://freelancing-gods.com/thinking-sphinx/v5/searching.html
     query = ThinkingSphinx::Query.escape('Article with an Index: 1')
