3cca62ca0f1147a87c14b1bc3a1007572a34ef71
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-01-12 01:49:12 +0300

    Remove Sphinx tails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,5 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.first(3)
-
-    # https://freelancing-gods.com/thinking-sphinx/v5/searching.html
-    query = ThinkingSphinx::Query.escape('Article with an Index: 1')
-    @sphinx_articles = Article.search(
-      query,
-      order: 'title ASC'
-    )
   end
 end
--- app/indices/article_index.rb
+++
@@ -1,9 +0,0 @@
-# 1. rake ts:configure
-# 2. docker exec rails7app-sphinx-1 indexer --all --config /opt/sphinx/conf/sphinx.conf
-# 3. Article.search('Article')
-ThinkingSphinx::Index.define :article, :with => :active_record do
-  indexes title, :sortable => true
-  indexes content
-
-  has created_at, updated_at
-end
