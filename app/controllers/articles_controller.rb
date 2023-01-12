d7f2290bc2220754645916e2ed15571e35c6db4c
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-01-12 20:53:56 +0300

    Chewy search

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,23 @@
 class ArticlesController < ApplicationController
   def index
+    # Example of ElasticSearch/Chewy search
+    @search_query = 'stench'
+    @found_articles = ArticlesIndex
+      .query(
+        query_string: {
+          query: @search_query,
+          fields: [:title, :content]
+      })
+      .highlight({
+        pre_tags: ['<b>'],
+        post_tags: ['</b>'],
+        fields: {
+          title: {},
+          content: {}
+        }
+      })
+      .limit(3)
+
     @articles = Article.first(3)
   end
 end
