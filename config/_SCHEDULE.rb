8bf9a8131e16eb45e5694e9c1efbe0131b525e74
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-01-19 00:15:55 +0300

    Add Whenever

---
+++ config/_SCHEDULE.rb
@@ -0,0 +1,27 @@
+# Learn more: http://github.com/javan/whenever
+#
+# How to initialize whenever
+# `bundle exec wheneverize .`
+#
+# How to start cron service
+# as ROOT: /etc/init.d/cron start
+#
+# How to start whenever
+# whenever --update-crontab --load-file config/_SCHEDULE.rb -i lucky
+#
+# How to see current crontab
+# crontab -l
+
+env :GEM_HOME, ENV['GEM_HOME']
+set :environment, ENV.fetch("RAILS_ENV") { 'development' }
+
+set :output,  {
+  standard: '/home/lucky/app/log/cron.log',
+  error: '/home/lucky/app/log/cron.error.log'
+}
+
+job_type :rails, "cd :path && :environment_variable=:environment bundle exec rails :task :output"
+
+every 1.minute do
+  rails "runner \"CurrencyRate.get('USD', 'EUR')\""
+end
---
+++ db/migrate/20230121113818_create_currency_rates.rb
@@ -0,0 +1,11 @@
+class CreateCurrencyRates < ActiveRecord::Migration[7.0]
+  def change
+    create_table :currency_rates do |t|
+      t.string :from
+      t.string :to
+      t.float :rate
+
+      t.timestamps
+    end
+  end
+end
