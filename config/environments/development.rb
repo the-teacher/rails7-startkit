8606110783020717db1d45765fd076b8b36f6b46
Ilya N. Zykin <ilya.zykin@toptal.com>
2023-02-04 14:59:08 +0300

    add default_url_options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,11 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # link_to helper requires this setting
+  config.action_mailer.default_url_options = {
+    host: 'localhost:3000'
+  }
+
   # Don't care if the mailer can't send.
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
