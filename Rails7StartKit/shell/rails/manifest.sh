701ec844e116cb2c240df801c9f0403d1477a5f7
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-03-18 16:20:59 +0300

    Update rails docker manifest script

--- Rails7StartKit/shell/rails/manifest.sh
+++ Rails7StartKit/shell/rails/manifest.sh
@@ -2,9 +2,11 @@
 #
 # source Rails7StartKit/shell/rails/manifest.sh
 
+docker manifest rm iamteacher/rails7:latest
+
 docker manifest create \
   iamteacher/rails7:latest \
   --amend iamteacher/rails7:amd64 \
   --amend iamteacher/rails7:arm64
 
-docker manifest push iamteacher/rails7:latest
+docker manifest push --purge iamteacher/rails7:latest
